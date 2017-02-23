3ca3df40f97907daa890d2850e42967f8d7da2bf
Francisco Vale <xico.vale@hotmail.com>
2017-02-23 16:50:33 +0000

    Set up database connection

--- Academia/src/dbconnect/DBConnect.java
+++ Academia/src/dbconnect/DBConnect.java
@@ -1,5 +1,41 @@
 package dbconnect;
 
-public class DBConnect {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class DBConnect {
+	private final String URL = "jdbc:mysql://localhost:3306/data";
+	private Connection conn;
+	
+	public DBConnect(){
+		connect();
+	}
+	
+	private void connect(){
+		try {
+			conn = DriverManager.getConnection(URL, "root", "password");
+		} catch (SQLException e) {
+			System.out.println("SQLException: " + e.getMessage());
+			System.out.println("SQLState: " + e.getSQLState());
+			System.out.println("VendorError: " + e.getErrorCode());
+		}
+	}
+	public Connection getConnection(){
+		return conn;
+	}
+	public void queryDb(String query){
+		try {
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery(query);
+			while (rs.next()) {
+				System.out.println("Product: " + rs.getString("productName"));
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+	}
 }
--- Academia/src/main/Main.java
+++ Academia/src/main/Main.java
@@ -1,10 +1,10 @@
 package main;
 
+import dbconnect.DBConnect;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
