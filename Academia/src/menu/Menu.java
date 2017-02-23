2e37f07533d86eff5165d948ad75dba2ad675a38
vanessasardinha <vanessamoraissardinha@gmail.com>
2017-02-23 11:08:28 +0000

    Class Menu

--- Academia/src/menu/Menu.java
+++ Academia/src/menu/Menu.java
@@ -1,5 +1,44 @@
 package menu;
 
+import java.util.Scanner;
+
 public class Menu {
 
+	public void SelectOption() {
+
+		Scanner input = new Scanner(System.in);
+		int option;
+		{
+			System.out.println("   Please select an option:");
+			System.out.println("-------------------------------");
+			System.out.println("[1] New client registration");
+			System.out.println("[2] New order");
+			System.out.println("[3] Export the list of clients to a file");
+			System.out.println("[4] Export the orders of a client to a text file");
+			System.out.println("[5] Import file to Database (binary file)");
+
+			option = input.nextInt();
+
+			switch (option) {
+
+			case 1:
+				int newCostumerNumber;
+				String newCostumerName;
+
+				break;
+
+			case 2:
+				break;
+
+			case 3:
+				break;
+
+			case 4:
+				break;
+
+			case 5:
+				break;
+			}
+		}
+	}
 }
