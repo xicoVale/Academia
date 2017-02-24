f2e3bcf3790f5f6da4e1bf745cba51c7c78fa4ff
Francisco Vale <xico.vale@hotmail.com>
2017-02-24 16:05:20 +0000

    Added register methods to Oders and OrderDetails.

--- Academia/src/tables/Tables.java
+++ Academia/src/tables/Tables.java
@@ -2,9 +2,14 @@ package tables;
 
 import java.util.ArrayList;
 
+import dbconnect.DBConnect;
+
 abstract public class Tables {
 
 	public abstract int getSize();
 	public abstract ArrayList<String> getAttributes();
 	public abstract void setAttributes();
+	public abstract void setConn(DBConnect conn);
+	public abstract DBConnect getConn();
+	public abstract void register();
 }
