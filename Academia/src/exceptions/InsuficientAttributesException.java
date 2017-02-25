841f8a2c8c814c835a4283fa6dc22de21de5c35a
Francisco Vale <xico.vale@hotmail.com>
2017-02-25 22:43:48 +0000

    Created an exception when a binary import file doesn't have enough attributes

---
+++ Academia/src/exceptions/InsuficientAttributesException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class InsuficientAttributesException extends Exception {
+	private static final long serialVersionUID = -253026366023353807L;
+	
+	public InsuficientAttributesException() {
+		super("O ficheiro não contem attributos suficientes");
+	}
+
+}
