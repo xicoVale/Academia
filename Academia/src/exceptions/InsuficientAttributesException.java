88ac33691700a2b87658faa66b8a10ace5fc47a7
Francisco Vale <xico.vale@hotmail.com>
2017-02-26 15:46:46 +0000

    Fixed typos in exception messages.

--- Academia/src/exceptions/InsuficientAttributesException.java
+++ Academia/src/exceptions/InsuficientAttributesException.java
@@ -4,7 +4,7 @@ public class InsuficientAttributesException extends Exception {
 	private static final long serialVersionUID = -253026366023353807L;
 	
 	public InsuficientAttributesException() {
-		super("O ficheiro não contem attributos suficientes");
+		super("O ficheiro não contem attributos suficientes.");
 	}
 
 }
